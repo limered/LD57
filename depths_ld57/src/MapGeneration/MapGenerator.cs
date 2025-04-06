75e7c0749ff495d1ffee23ac6d7a64bb8140ab7f
limered <me@limered.de>
2025-04-06 15:46:43 +0200

    refactors map generation process
    Moves map generation logic to a separate function for better control and integration.
    Adds progress reporting during map generation using an Action delegate.
    Exposes map images for collision, color, and dirt.
    Tweaks default noise settings and adds erosion.
    Removes process-based map generation.

--- depths_ld57/src/MapGeneration/MapGenerator.cs
+++ depths_ld57/src/MapGeneration/MapGenerator.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Threading;
 using depths_ld57.MapGeneration.Steps;
 using Godot;
 
@@ -6,56 +8,79 @@ namespace depths_ld57.MapGeneration;
 
 public partial class MapGenerator : Node
 {
-	[Export] public Vector2I MapSize = new(1024, 1024);
-	[Export] public FastNoiseLite Noise;
-	[Export] public float WallThreshold = 0.5f;
-	[Export] public bool Dilate = true;
-	[Export] public bool Erode = false;
-	[Export] public int DilateKernelSize = 4; 
-	[Export] public int ErodeKernelSize = 4; 
-	[Export] public float UpscaleFactor = 8f;
-	
-	private Sprite2D _mapSprite;
-	private Texture2D _texture;
-	
-	private MapGenerationContext _context;
-	private readonly List<IMapGenerationStep> _steps = new();
-	
-	public override void _Ready()
-	{
-		_mapSprite = GetNode<Sprite2D>("MapPreview");
+    private readonly List<IMapGenerationStep> _steps = new();
 
-		_context = new MapGenerationContext
-		{
-			WorkingImage = Image.CreateEmpty(MapSize.X, MapSize.Y, false, Image.Format.Rgbaf)
-		};
-		
-		_steps.Add(new BaseNoiseGenerationStep(MapSize, Noise));
-		_steps.Add(new ImageProcessingStep(MapSize));
-		_steps.Add(new BrainFormStep(MapSize));
-		_steps.Add(new FloodTestStep(MapSize));
-		_steps.Add(new UpscaleStep(MapSize));
-		_steps.Add(new SplitMapStep(MapSize));
-	}
-	
-	public override void _Process(double delta)
-	{
-		_context.Dilated = Dilate;
-		_context.Eroded = Erode;
-		_context.WallThreshold = WallThreshold;
-		_context.DilationKernel = DilateKernelSize;
-		_context.ErosionKernel = ErodeKernelSize;
-		_context.UpscaleFactor = UpscaleFactor;
-		
-		if(Input.IsKeyPressed(Key.G))
-		{
-			foreach (var step in _steps)
-			{
-				step.Generate(_context);
-			}
-			_texture = ImageTexture.CreateFromImage(_context.WorkingImage);
-		}
-		
-		_mapSprite.Texture = _texture;
-	}
+    private MapGenerationContext _context;
+
+    private Sprite2D _mapSprite;
+    private Texture2D _texture;
+    [Export] public bool Dilate = true;
+    [Export] public int DilateKernelSize = 2;
+    [Export] public bool Erode = true;
+    [Export] public int ErodeKernelSize = 6;
+    [Export] public Vector2I MapSize = new(1024, 1024);
+    [Export] public FastNoiseLite Noise = new();
+    [Export] public float UpscaleFactor = 8f;
+    [Export] public float WallThreshold = 0.67f;
+
+    public Image CollisionMap => _context.CollisionMap;
+    public Image ColorMap => _context.ColorMap;
+    public Image DirtMap => _context.DirtMap;
+
+    public Action<float> OnProgressChanged;
+    
+    public override void _Ready()
+    {
+        _mapSprite = GetNode<Sprite2D>("MapPreview");
+
+        Noise.NoiseType = FastNoiseLite.NoiseTypeEnum.SimplexSmooth;
+        Noise.Frequency = 0.015f;
+        Noise.Seed = GD.RandRange(0, 10000);
+
+        Noise.SetFractalType(FastNoiseLite.FractalTypeEnum.Ridged);
+        Noise.FractalOctaves = 2;
+        Noise.FractalLacunarity = 2.0f;
+        Noise.FractalGain = 0.6f;
+
+        _context = new MapGenerationContext
+        {
+            WorkingImage = Image.CreateEmpty(MapSize.X, MapSize.Y, false, Image.Format.Rgbaf)
+        };
+
+        _steps.Add(new BaseNoiseGenerationStep(MapSize, Noise));
+        _steps.Add(new ImageProcessingStep(MapSize));
+        _steps.Add(new BrainFormStep(MapSize));
+        _steps.Add(new FloodTestStep(MapSize));
+        _steps.Add(new UpscaleStep(MapSize));
+        _steps.Add(new SplitMapStep(MapSize));
+    }
+
+    public void GenerateMap()
+    {
+        _context.Dilated = Dilate;
+        _context.Eroded = Erode;
+        _context.WallThreshold = WallThreshold;
+        _context.DilationKernel = DilateKernelSize;
+        _context.ErosionKernel = ErodeKernelSize;
+        _context.UpscaleFactor = UpscaleFactor;
+
+        for (var i = 0; i < _steps.Count; i++)
+        {
+            var step = _steps[i];
+            step.Generate(_context);
+            OnProgressChanged?.Invoke(i/(_steps.Count - 1f));
+            GD.Print(step.GetType().Name + " done");
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsKeyPressed(Key.G)) GenerateMap();
+
+        if (Input.IsKeyPressed(Key.K))
+        {
+            _texture = ImageTexture.CreateFromImage(_context.WorkingImage);
+            _mapSprite.Texture = _texture;
+        }
+    }
 }
