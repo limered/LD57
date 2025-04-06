9b220d3e5d1f075dd29506c74f1c9908bfab0a13
limered <me@limered.de>
2025-04-06 21:36:01 +0200

    rotates dirt particles for more variety

--- depths_ld57/src/Dirt/DirtParticle.cs
+++ depths_ld57/src/Dirt/DirtParticle.cs
@@ -4,6 +4,10 @@ namespace depths_ld57.Dirt;
 
 public partial class DirtParticle : Node2D
 {
-    public Texture2D DirtTexture {set => GetNode<Sprite2D>("Image").Texture = value;} 
- 
+    public Texture2D DirtTexture {set => GetNode<Sprite2D>("Image").Texture = value;}
+
+    public override void _Ready()
+    {
+        GetNode<Sprite2D>("Image").Rotation = GD.Randf() * 360f;
+    }
 }
