2558ea4885ffa2185875d66f90cda3fd079db477
limered <me@limered.de>
2025-04-06 19:25:31 +0200

    adds performant tail remove list

---
+++ depths_ld57/src/Utils/TailRemoveList.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace depths_ld57.Utils;
+
+public class TailRemoveList<T>
+{
+    private readonly List<T> _list;
+
+    public TailRemoveList(int length)
+    {
+        _list = new List<T>(length);
+    }
+    public void Add(T value)
+    {
+        _list.Add(value);
+    }
+    
+    public void Remove(int index)
+    {
+        if (index == Count - 1)
+        {
+            _list.RemoveAt(index);
+            return;
+        }
+        var lastElement = _list.Last();
+        _list[index] = lastElement;
+        _list.RemoveAt(_list.Count - 1);
+    }
+    
+    public int Count => _list.Count;
+
+    public T this[int i]
+    {
+        get => _list[i];
+        set => _list[i] = value;
+    }
+}
