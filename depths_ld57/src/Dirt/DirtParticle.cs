cdfedad8a8f99275b9c9ac7faf4307861fc30737
Michael Scherbakow <ryupold@gmail.com>
2025-04-07 01:48:02 +0200

    makes dirt particles explode

--- depths_ld57/src/Dirt/DirtParticle.cs
+++ depths_ld57/src/Dirt/DirtParticle.cs
@@ -11,19 +11,44 @@ public partial class DirtParticle : Area2D
     [Export] public CollisionShape2D Collider;
 
     public Texture2D DirtTexture { set => Sprite.Texture = value; }
+    private CpuParticles2D explodeParticles;
+
+    private double disposeAfter = 2f;
 
     public override void _Ready()
     {
         Sprite.Rotation = GD.Randf() * 360f;
         ((CircleShape2D)Collider.Shape).Radius = (Sprite.Texture.GetWidth() + Sprite.Texture.GetHeight()) / 5f;
+        (collider.Shape as CircleShape2D).Radius = (image.Texture.GetWidth() + image.Texture.GetHeight()) / 5f;
+        explodeParticles = GetNode<CpuParticles2D>("CPUParticles2D");
     }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+        if (Health <= 0 && disposeAfter > 0)
+        {
+            disposeAfter -= delta;
+
+            if (disposeAfter <= 0)
+            {
+                QueueFree();
+            }
+        }
+
+    }
+
 
     public void Damage()
     {
+        if (Health <= 0) return;
+
         Health--;
         if (Health <= 0)
         {
-            QueueFree();
+            explodeParticles.Emitting = true;
+            GetNode<Sprite2D>("Image")?.QueueFree();
         }
     }
 }
