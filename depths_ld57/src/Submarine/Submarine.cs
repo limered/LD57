793d29eb9e4dd7b801e6cfad3c3fdd58366d9647
Michael Scherbakow <ryupold@gmail.com>
2025-04-06 21:25:55 +0200

    add smooth camera follow

--- depths_ld57/src/Submarine/Submarine.cs
+++ depths_ld57/src/Submarine/Submarine.cs
@@ -12,27 +12,51 @@ public partial class Submarine : RigidBody2D
 	[Export]
 	public float WaterDrag { get => LinearDamp; set => LinearDamp = value; }
 
+	public float SubmarineRadius { get; private set; }
+
+	[Export]
+	public float SubmarineLookahead { get; set; } = 10f;
+
+
 	private AnimatedSprite2D _sprite;
 
-	private Image collisionMap;
+	private CollisionChecker collisionChecker;
+
 
 	public override void _Ready()
 	{
 		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		EventBus.Register<MapGeneratedEvent>((evt) => {
+		EventBus.Register<MapGeneratedEvent>((evt) =>
+		{
 			var mapGenerator = GetNode<MapGenerator>("/root/LevelGenerator");
-			collisionMap = mapGenerator.CollisionMap;
+			collisionChecker = new(mapGenerator.CollisionMap);
 		});
+		var capsule = (CapsuleShape2D)GetNode<CollisionShape2D>("CollisionShape2D").Shape;
+		SubmarineRadius = (capsule.Radius + capsule.Height) / 2f;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		HandleMovement(delta);
+		var direction = HandleMovement(delta);
 		Mathf.Sign(LinearVelocity.X);
 		_sprite.FlipH = LinearVelocity.X > 0;
+		HandleCollision(direction);
 	}
 
-	private void HandleMovement(double delta)
+	private void HandleCollision(Vector2 moveDirection)
+	{
+		if (collisionChecker == null)
+			return;
+		if (moveDirection == Vector2.Zero) return;
+
+		if (collisionChecker.IsCollision(GlobalPosition + moveDirection * (SubmarineRadius + SubmarineLookahead)))
+		{
+			LinearVelocity = Vector2.Zero;
+			GD.Print("Collision detected with wall");
+		}
+	}
+
+	private Vector2 HandleMovement(double delta)
 	{
 		var direction = Vector2.Zero;
 		if (Input.IsActionPressed("move_up"))
@@ -49,5 +73,30 @@ public partial class Submarine : RigidBody2D
 			direction = direction.Normalized();
 			ApplyCentralForce(direction * Acceleration * (float)delta);
 		}
+		return direction;
 	}
+}
+
+
+public class CollisionChecker
+{
+	public readonly Image collisionMap;
+	public readonly int width, height;
+
+	public CollisionChecker(Image collisionMap)
+	{
+		this.collisionMap = collisionMap;
+		width = collisionMap.GetWidth();
+		height = collisionMap.GetHeight();
+	}
+
+	public bool IsCollision(Vector2 position)
+	{
+		if (position.X <= -width / 2 || position.X >= width / 2 || position.Y <= -height / 2 || position.Y >= height / 2)
+			return true;
+
+		var pixel = collisionMap.GetPixel((int)position.X + width / 2, (int)position.Y + height / 2);
+		return pixel == new Color(0, 0, 0, 0);
+	}
+
 }
