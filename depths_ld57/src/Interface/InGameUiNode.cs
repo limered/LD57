aba3f5930c39186b766c220993458f9e358ae8fb
limered <me@limered.de>
2025-04-08 01:49:16 +0200

    add fancy progress shader

--- depths_ld57/src/Interface/InGameUiNode.cs
+++ depths_ld57/src/Interface/InGameUiNode.cs
@@ -7,12 +7,16 @@ namespace depths_ld57.Interface;
 public partial class InGameUiNode : Control
 {
     [Export] public Label ScoreLabel;
+    [Export] public Control ScoreControl;
 
+    private ShaderMaterial _scoreShader;
+    
     private Game _game;
 
     public override void _Ready()
     {
         _game = GetNode<Game>("/root/Game");
+        _scoreShader = ScoreControl.Material as ShaderMaterial;
     }
 
     public override void _Process(double delta)
@@ -20,12 +24,13 @@ public partial class InGameUiNode : Control
         if(_game.State == GameState.Running)
         {
             SetVisible(true);
-            ScoreLabel.Text = "Percent Done: " + ((1.0 - ScoreStore.PercentLeft) * 100).ToString("0.00") + "%";
+            var percent = 1.0 - ScoreStore.PercentLeft;
+            ScoreLabel.Text = (percent * 100).ToString("0.00") + "%";
+            _scoreShader.SetShaderParameter("progress", (float)percent);
         }
         else
         {
             SetVisible(false);
-            ScoreLabel.Text = "";
         }
     }
 }
